a564905b4a07747005cc84c9b010632dbbc9817c
Saumil Upadhyay <saumil.upadhyay@gmail.com>
2026-01-18 15:31:42 +0530

    changes in requirements.txt

--- University-Time-table-scheduler-main/backend/scheduler/settings.py
+++ University-Time-table-scheduler-main/backend/scheduler/settings.py
@@ -154,9 +154,8 @@ USE_TZ = True
 # ------------------------
 # STATIC FILES
 # ------------------------
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # ------------------------
 # DEFAULT PRIMARY KEY FIELD TYPE
